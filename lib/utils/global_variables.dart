e718e0264d16b6e73f6254e8d430613c35d7a826
phods <andregk@gmail.com>
2022-11-20 22:32:33 +0000

    name change

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
-import 'package:instagram_flutter/screens/feedScreen.dart';
+import 'package:instagram_flutter/screens/feed_screen.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
 import 'package:instagram_flutter/screens/profile_screen.dart';
 import 'package:instagram_flutter/screens/search_screen.dart';
